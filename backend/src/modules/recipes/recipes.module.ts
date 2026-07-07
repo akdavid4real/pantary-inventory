a4c0f41d849b01bde5e101458b125a351aceb883
Ak David <120152054+akdavid4real@users.noreply.github.com>
2026-07-07 15:37:22 +0100

    Add recipes module

---
+++ backend/src/modules/recipes/recipes.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { IngredientsModule } from '../ingredients/ingredients.module';
+import { RecipesController } from './recipes.controller';
+import { RecipesService } from './recipes.service';
+
+@Module({
+  imports: [IngredientsModule],
+  controllers: [RecipesController],
+  providers: [RecipesService],
+  exports: [RecipesService],
+})
+export class RecipesModule {}

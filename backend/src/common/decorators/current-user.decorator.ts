32210b6f48393251d4d78c9b34b633f83804b01a
Ak David <120152054+akdavid4real@users.noreply.github.com>
2026-07-07 15:29:13 +0100

    Add current user decorator

---
+++ backend/src/common/decorators/current-user.decorator.ts
@@ -0,0 +1,7 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { RequestWithUser } from '../types/request-user';
+
+export const CurrentUser = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
+  const request = ctx.switchToHttp().getRequest<RequestWithUser>();
+  return request.user;
+});

a7e88430cbad56a28d8831be780e8c3d760dd125
Ak David <120152054+akdavid4real@users.noreply.github.com>
2026-07-07 15:46:08 +0100

    Add nutrition controller

---
+++ backend/src/modules/nutrition/nutrition.controller.ts
@@ -0,0 +1,38 @@
+import { Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { CurrentUser } from '../../common/decorators/current-user.decorator';
+import { RequestUser } from '../../common/types/request-user';
+import { NutritionWeekQueryDto } from './dto/nutrition.dto';
+import { NutritionService } from './nutrition.service';
+
+@ApiTags('Nutrition')
+@ApiBearerAuth()
+@Controller('nutrition')
+export class NutritionController {
+  constructor(private readonly nutritionService: NutritionService) {}
+
+  @Get('recipe/:recipeId')
+  recipeNutrition(@Param('recipeId') recipeId: string) {
+    return this.nutritionService.recipeNutrition(recipeId);
+  }
+
+  @Post('calculate-recipe/:recipeId')
+  calculateRecipe(@Param('recipeId') recipeId: string) {
+    return this.nutritionService.calculateRecipe(recipeId);
+  }
+
+  @Get('day/:date')
+  daySummary(@CurrentUser() user: RequestUser, @Param('date') date: string) {
+    return this.nutritionService.daySummary(user.id, date);
+  }
+
+  @Get('week')
+  weekSummary(@CurrentUser() user: RequestUser, @Query() query: NutritionWeekQueryDto) {
+    return this.nutritionService.weekSummary(user.id, query.date);
+  }
+
+  @Get('meal-plan/week')
+  mealPlanWeekSummary(@CurrentUser() user: RequestUser, @Query() query: NutritionWeekQueryDto) {
+    return this.nutritionService.weekSummary(user.id, query.date);
+  }
+}

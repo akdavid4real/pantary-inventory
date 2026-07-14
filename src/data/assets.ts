3e9ddeaa26c32e34c87499501f7650f605c27931
akdavid <akdavid4real@gmail.com>
2026-07-14 16:58:09 +0100

    Remove Chef Sahmmy kitchen image and update asset references; add Open Graph and Twitter meta tags in index.html

--- src/data/assets.ts
+++ src/data/assets.ts
@@ -4,7 +4,6 @@ import recipeBowl from "../../assets/asset_6.png";
 import groceryBag from "../../assets/asset_2.png";
 import herbDetail from "../../assets/asset_3.png";
 import cookingMode from "../../assets/asset_1.png";
-import chefSahmmyKitchen from "../../assets/chef-sahmmy-kitchen.png";
 import logoMark from "../../assets/carrots.png";
 
 export const assets = {
@@ -14,7 +13,7 @@ export const assets = {
   grocery: groceryBag,
   herbs: herbDetail,
   cooking: cookingMode,
-  chefSahmmy: chefSahmmyKitchen,
+  chefSahmmy: "/og-preview.png",
   logo: logoMark,
 };
 
